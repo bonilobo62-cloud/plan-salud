ea479febb75aaf6891b18ccaa26ed96844b5be20
boni lobo <bonilobo62@gmail.com>
2026-06-14 14:06:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plan-salon-v1';
+const CACHE_NAME = 'plan-salon-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,29 +9,42 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
   );
+  // Forzar activación inmediata
+  self.skipWaiting();
 });
 
 // Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
 
-// Activate
+// Activate - limpiar caches antiguos
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
+  // Tomar control inmediato
+  return self.clients.claim();
 });
